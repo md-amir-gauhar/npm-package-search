b60430b980c8931b1f6767632f76902500fc0c0e
md-amir-gauhar <gauharamir15@gmail.com>
2021-06-30 23:40:39 +0530

    separate interfaces for actions

--- src/redux/reducers/packagesReducer.ts
+++ src/redux/reducers/packagesReducer.ts
@@ -4,7 +4,21 @@ interface PackageState {
   data: string[];
 }
 
-const reducer = (state: PackageState, action: any): PackageState => {
+interface SearchPackagesAction {
+  type: 'search_packages';
+}
+
+interface SearchPackagesSuccessAction {
+  type: 'search_packages_success';
+  payload: string[];
+}
+
+interface SearchPackagesErrorAction {
+  type: 'search_packages_error';
+  payload: string;
+}
+
+const reducer = (state: PackageState, action): PackageState => {
   switch (action.type) {
     case 'search_packages':
       return { loading: true, error: null, data: [] };
